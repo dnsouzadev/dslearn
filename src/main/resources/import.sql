7c4c8cb12c1e166ba64bfa02526dadfd16150ee9
Daniel Souza <workdndsza@gmail.com>
2024-12-16 20:16:13 -0300

    criando a offer e fazendo os relacionamentos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Spring Boot', 'https://img.icons8.com/color/48/000000/spring-logo.png', 'https://img.icons8.com/color/48/000000/spring-logo.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1st Edition', TIMESTAMP WITH TIME ZONE '2024-06-01T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-06-30T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2nd Edition', TIMESTAMP WITH TIME ZONE '2024-01-01T03:00:00Z', TIMESTAMP WITH TIME ZONE '2025-01-30T03:00:00Z', 1);
